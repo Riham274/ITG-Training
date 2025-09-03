66bf63670c127f73ce37f73866b2d5896c59c213
Riham274 <riham.ararawi274@gmail.com>
2025-09-03 20:07:21 +0300

    Implement password strength meter and indicators

---
+++ Task18/task18.js
@@ -0,0 +1,57 @@
+$(document).ready(function() {
+    $('#password').on('input', function(){
+        let value = $('#password').val();
+        let score = 0;
+        let strength = "None";
+        let color = "grey";
+
+        score += Math.min(Math.floor(value.length / 4), 4);
+
+        if (/[a-z]/.test(value))
+            score += 2;
+
+        if (/[A-Z]/.test(value))
+            score += 2;
+
+        if (/[0-9]/.test(value))
+            score += 2;
+
+        if (/[^a-zA-Z0-9]/.test(value))
+            score += 2;
+
+
+       if (score <= 5) {
+        strength = "Weak";
+        color = "red";
+       }
+       else if (score >= 6 && score <= 8) {
+        strength = "Medium";
+        color = "orange";
+       }
+
+       else if (score > 8 ) {
+        strength = "Strong";
+        color = "green";
+       }
+
+       $("#progress").css("width", (score / 12) * 100 + "%")
+                     .css("background-color" , color);
+
+       $("#strength").text("Strength: " + strength).css("color" , color);
+       $("#length").text("length: " + value.length);
+
+       update("#lowercase", /[a-z]/.test(value));
+       update("#uppercase", /[A-Z]/.test(value));
+       update("#number", /[0-9]/.test(value));
+       update("#special", /[^a-zA-Z0-9]/.test(value));
+
+
+       function update(id , condition) {
+        let mark = condition ? "✓" : "✗";
+        let color = condition ? "green" : "red";
+        $(id + " .check").text(mark).css("color", color);
+       }
+
+
+    } )
+})
